2e488d03c30fdb00df38806bc491950bd83ccdc6
Millena Fernandes <100374530+xmillena@users.noreply.github.com>
2025-10-24 09:23:27 -0300

    Refactor dashboard code for better readability

--- script/dashboard_final.py
+++ script/dashboard_final.py
@@ -27,7 +27,6 @@ INSTRUMENTOS = {
 
 @st.cache_data
 def load_prep_data():
-    """Carregando os dados"""
     # Carrega o CSV final
     if not os.path.exists(OUTPUT_FILE):
         # Se o arquivo não existe, roda o ETL antes de tentar carregar
@@ -42,54 +41,81 @@ def load_prep_data():
     try:
         df = pd.read_csv(OUTPUT_FILE, sep=';', encoding='utf-8-sig')
         return df
-
     
     except Exception as e: 
         st.error(f'Erro ao ler arquivo CSV: {e}')
         return pd.DataFrame()         
 
-
-def show_df(df):
-    instrumento_selecionado = st.session_state.get('instrumento_selecionado', "Plano de Saúde")
-    cols = st.columns(6)
-
-    for i, (nome, tipo) in enumerate(INSTRUMENTOS.items()):
-        current_type = "primary" if nome == instrumento_selecionado else "secondary"
-        
-        if cols[i].button(nome, type=current_type, use_container_width=True):
-            st.session_state['instrumento_selecionado'] = nome
-            st.rerun()
-
-
-    #Tabela Detalhada para o Instrumento Selecionado
-    st.markdown("---")
-    st.subheader(instrumento_selecionado)
-    
-    # Filtra o DataFrame apenas para o instrumento selecionado
-    df_instrumento = df[df['INSTRUMENTO'] == instrumento_selecionado].copy()
-    
-    if df_instrumento.empty:
-        st.info(f"Nenhum registro encontrado para '{instrumento_selecionado}' com os filtros aplicados.")
-        return
-    
-    if instrumento_selecionado == "Plano de Saúde" and "ANO" in df_instrumento.columns:
-        df_instrumento = df_instrumento.drop(columns=["ANO"])
-
-    df_instrumento= df_instrumento.drop(columns = ["DATA_PROCESSAMENTO"])
-    st.dataframe(df_instrumento, use_container_width=True,height=700,)
-
 def create_filters(df):
-    st.sidebar.header("Selecione o município")
 
-    municipio_list = ['Todos'] + sorted(df['MUNICIPIO'].dropna().unique().tolist())
+    municipio_list = ['SAO LUIS'] + sorted(df['MUNICIPIO'].dropna().unique().tolist())
     municipio_selecionado = st.sidebar.selectbox('Município', municipio_list)
+
+    periodo_list = sorted(df['FASE'].dropna().unique().tolist())
+    periodo_selecionado = st.sidebar.selectbox('Período', periodo_list)
 
     if municipio_selecionado != 'Todos':
         df = df[df['MUNICIPIO'] == municipio_selecionado].copy()
+    
+    if periodo_selecionado:
+        df = df[df['FASE'] == periodo_selecionado].copy()
+    
+    st.subheader(f"Resultado para {municipio_selecionado} - Período {periodo_selecionado}")
+    st.markdown("---")
 
     return df
 
-     
+
+
+def show_df(df):
+    col_header_inst, col_header_periodo, col_header_status = st.columns([1, 1, 1])
+    col_header_inst.markdown("**INSTRUMENTO**")
+    col_header_periodo.markdown("**ANO**")
+    col_header_status.markdown("**STATUS**")
+
+    plano_saude = df[df['INSTRUMENTO'] == 'Plano de Saúde']
+    
+    if not plano_saude.empty:
+        plano_status = plano_saude.iloc[0]['STATUS']
+        plano_periodo = plano_saude.iloc[0]['FASE']
+
+        col_inst, col_ano, col_status = st.columns([1, 1, 1])
+        col_inst.markdown(f"**Plano de Saúde**")
+        col_ano.markdown(plano_periodo)
+        col_status.markdown(f"{plano_status}")
+    
+    st.markdown("---")
+
+    df_sem_plano = df[df['INSTRUMENTO'] != 'Plano de Saúde'].copy()
+    
+    # Converter ANO para inteiro
+    df_sem_plano['ANO'] = df_sem_plano['ANO'].astype(float).astype(int)
+    
+    anos = sorted(df_sem_plano['ANO'].unique())
+    
+    for idx, ano in enumerate(anos):
+        df_ano = df_sem_plano[df_sem_plano['ANO']==ano].sort_values(by=['INSTRUMENTO'])
+       
+        for i, row in df_ano.iterrows():
+            instrumento_nome = row['INSTRUMENTO']
+            instrumento_status = row['STATUS']
+            instrumento_ano = row['ANO'] 
+            
+            col_inst, col_ano, col_status = st.columns([1, 1, 1])
+            
+            
+            col_inst.markdown(f"**{instrumento_nome}**")
+            col_ano.markdown(f"{instrumento_ano}")
+            col_status.markdown(f"{instrumento_status}")
+        
+        
+        if idx < len(anos) - 1:
+            st.markdown("---")
+
+        
+
+
+
 def main():
     
     df_dados = load_prep_data()
@@ -102,20 +128,16 @@ def main():
     layout="wide",
     initial_sidebar_state="expanded")
 
-    st.title(" Monitoramento dos Instrumentos de Planejamento e Gestão em Saúde no Maranhão")
-    st.markdown("---")
-    
-    if 'instrumento_selecionado' not in st.session_state:
-        st.session_state['instrumento_selecionado'] = "Plano de Saúde"
+    st.title(" Monitoramento dos Instrumentos de Planejamento e Gestão em Saúde no Maranhão 📈")
 
     if 'DATA_PROCESSAMENTO' in df_dados.columns and not df_dados.empty:
         data_atualizacao = pd.to_datetime(df_dados['DATA_PROCESSAMENTO'], errors='coerce').max()
         data_atualizacao = data_atualizacao.strftime("%d/%m/%Y")
-        st.info(f"**Última atualização da planilha:** {data_atualizacao}")
+        st.sidebar.info(f"**📅 Última atualização da planilha:** {data_atualizacao}")
  
     
     df_filtrado = create_filters(df_dados)
-        
+
     show_df(df_filtrado)
 
 
